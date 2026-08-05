813640a6885fc5c65f811e6e4ec3ee9b18a6045b
Manuel Paul <139383972+manuelp3@users.noreply.github.com>
2026-08-04 23:39:34 -0400

    Add: implement-prefix-tree - submission-6

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-6.py
@@ -0,0 +1,38 @@
+class TreeNode():
+    def __init__(self):
+        self.children = {}
+        self.flag = False
+
+class PrefixTree:
+
+    def __init__(self):
+        self.root = TreeNode()
+
+    def insert(self, word: str) -> None:
+        current = self.root
+        for i in range(len(word)):
+            char = word[i]
+            if char in current.children.keys():
+                current = current.children[char]
+            else:
+                current.children[char] = TreeNode()
+                current = current.children[char]
+        current.flag = True
+
+
+    def search(self, word: str) -> bool:
+        current = self.root
+        for char in word:
+            if char not in current.children.keys():
+                return False
+            current = current.children[char]
+        return current.flag
+
+    def startsWith(self, prefix: str) -> bool:
+        current = self.root
+        for char in prefix:
+            if char not in current.children.keys():
+                return False
+            current = current.children[char]    
+        return True
+        
