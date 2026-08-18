acfaf56315bd6c5858e601c3ef67baaeb37800ef
Manuel Paul <139383972+manuelp3@users.noreply.github.com>
2026-08-18 12:43:25 -0400

    Add: valid-tree - submission-4

---
+++ Data Structures & Algorithms/valid-tree/submission-4.py
@@ -0,0 +1,26 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        graph = {}
+        for i in range(n):
+            graph[i] = []
+        
+        for first, second in edges:
+            graph[first].append(second)
+            graph[second].append(first)
+    
+        visited = set()
+
+        def dfs(current, prev):
+            if current in visited:
+                return False
+            visited.add(current)
+            if not graph[current]:
+                return True
+            for neighbor in graph[current]:
+                if neighbor == prev:
+                    continue
+                if dfs(neighbor, current) == False:
+                    return False
+            graph[current] = []
+            return True
+        return dfs(0, -1) and len(visited) == n
